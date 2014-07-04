e63f5e65fac55c35ecaeef01245652c12c87cee0
Mike Perry <s14.aperry@gmail.com>
2014-07-03 22:40:00 -0400

    Minor change

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var path = require('path');
 var app = express();
 
 // all environments
-//app.set('port', process.env.PORT || 3000);
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000); 
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.set('views', path.join(__dirname, 'views'));
